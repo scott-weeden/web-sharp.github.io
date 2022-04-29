353547ee1d26d39bc54fed7cea47730eca4c5f18
Web Sharp <github@websharpstudios.com>
2022-04-29 13:46:40 -0700

    Update wentdaddy.sh

--- wentdaddy.sh
+++ wentdaddy.sh
@@ -1,2 +1,2 @@
 
-/bin/bash -c "env IP4=$(curl -fsSL4 ifconfig.co);env IP6=$(curl -fsSL6 ifconfig.co);hostname | sed 's/local/websharpstudios.com $IP4/'"
+/bin/env $SHELL -c "env IP4=$(curl -fsSL4 ifconfig.co);env IP6=$(curl -fsSL6 ifconfig.co);hostname | sed 's/local/websharpstudios.com $IP4/'"
